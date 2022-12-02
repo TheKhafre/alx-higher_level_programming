d108a78218949ba144a7c6fb600a8ad4f1938416
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2022-12-02 02:03:34 +0100

    added execution permission and updated the codes

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
+    """Match bytecode provided"""
     from magic_calculation_102 import add, sub
+
     if a < b:
-        sum = add(a, b)
+        c = add(a, b)
         for i in range(4, 6):
-            sum = add(sum, i)
-        return sum
+            c = add(c, i)
+        return (c)
+
     else:
-        return sub(a, b)
+        return (sub(a, b))
